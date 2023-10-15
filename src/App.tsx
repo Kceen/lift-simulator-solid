78b96e97830aa7ad160005951012d03749ae6126
Nikola Fimić <nikolafimic01@gmail.com>
2023-10-15 23:14:25 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { For, createEffect, createSignal } from "solid-js";
-import "./App.css";
-import { ICommand, ILiftState } from "./types";
+import { For, createEffect, createSignal } from 'solid-js'
+import './App.css'
+import { ICommand, ILiftState } from './types'
 
 function App() {
-  let executionInProgress = false;
+  let executionInProgress = false
   const [liftState, setLiftState] = createSignal<ILiftState>({
     currentFloor: 1,
     openedDoors: false,
-  });
-  const [commands, setCommands] = createSignal<ICommand[]>([]);
+  })
+  const [commands, setCommands] = createSignal<ICommand[]>([])
 
   createEffect(() => {
-    console.log(commands());
-  });
+    console.log(commands())
+  })
 
   function executeCommand() {
-    executionInProgress = true;
+    executionInProgress = true
 
     const intervalId = setInterval(() => {
-      const commandsTemp = [...commands()];
-      const nextCommand = commandsTemp.shift();
+      const commandsTemp = [...commands()]
+      const nextCommand = commandsTemp.shift()
 
-      if (nextCommand?.type === "GO_TO_FLOOR") {
+      if (nextCommand?.type === 'GO_TO_FLOOR') {
         if (liftState().openedDoors) {
           setLiftState((prevLiftState) => ({
             ...prevLiftState,
             openedDoors: false,
-          }));
+          }))
 
-          return;
+          return
         }
 
         if (nextCommand.data === liftState().currentFloor) {
@@ -36,69 +36,69 @@ function App() {
             ...prevLiftState,
             currentFloor: nextCommand?.data!,
             openedDoors: true,
-          }));
+          }))
 
           // COMMAND IS DONE, REMOVE IT FROM COMMANDS ARRAY
-          setCommands(commandsTemp);
+          setCommands(commandsTemp)
         }
         if (nextCommand.data! > liftState().currentFloor) {
           setLiftState((prevLiftState) => ({
             ...prevLiftState,
             currentFloor: prevLiftState.currentFloor + 1,
-          }));
+          }))
         }
         if (nextCommand.data! < liftState().currentFloor) {
           setLiftState((prevLiftState) => ({
             ...prevLiftState,
             currentFloor: prevLiftState.currentFloor - 1,
-          }));
+          }))
         }
       }
 
       if (commands().length === 0) {
-        executionInProgress = false;
-        clearInterval(intervalId);
+        executionInProgress = false
+        clearInterval(intervalId)
       }
-    }, 1000);
+    }, 1000)
   }
 
   return (
     <>
       <div>
         <p>current floor - {liftState().currentFloor}</p>
-        <p>opened doors - {liftState().openedDoors ? "true" : "false"}</p>
+        <p>opened doors - {liftState().openedDoors ? 'true' : 'false'}</p>
       </div>
 
-      <div class="building relative mb-24">
-        <div class="floors">
+      <div class='building relative mb-24'>
+        <div class='floors'>
           <For each={[5, 4, 3, 2, 1]}>
             {(floor) => (
-              <div class="relative">
-                <div class="w-20 h-28 border border-black"></div>
-                <div class="absolute top-0 -left-4">{floor}</div>
+              <div class='relative'>
+                <div class='w-20 h-28 border border-black'></div>
+                <div class='absolute top-0 -left-4'>{floor}</div>
               </div>
             )}
           </For>
         </div>
         <div
           style={{ bottom: `${liftState().currentFloor * 7 - 7}rem` }}
-          class="lift absolute w-20 h-28 bottom-0 transition-all ease-linear duration-1000"
+          class='lift absolute w-20 h-28 bottom-0 transition-all ease-linear duration-1000'
         >
           <div
             style={{
               transform: `${
-                liftState().openedDoors ? "translateX(-2rem)" : "translateX(0)"
+                liftState().openedDoors ? 'translateX(-2rem)' : 'translateX(0)'
               }`,
             }}
-            class="w-1/2 h-full bg-black inline-block duration-1000"
+            class='w-1/2 h-full bg-black inline-block duration-1000'
           ></div>
           <div
             style={{
               transform: `${
-                liftState().openedDoors ? "translateX(2rem)" : "translateX(0)"
+                liftState().openedDoors ? 'translateX(-4rem)' : 'translateX(0)'
               }`,
             }}
-            class="w-1/2 h-full bg-black inline-block duration-1000"
+            class='w-1/2 h-full bg-black inline-block duration-1000'
           ></div>
         </div>
       </div>
@@ -107,15 +107,15 @@ function App() {
         <For each={[1, 2, 3, 4, 5]}>
           {(buttonNumber) => (
             <button
-              class="border p-6"
+              class='border p-6'
               onclick={() => {
                 setCommands((prevCommands) => [
                   ...prevCommands,
-                  { type: "GO_TO_FLOOR", data: buttonNumber },
-                ]);
+                  { type: 'GO_TO_FLOOR', data: buttonNumber },
+                ])
 
                 if (!executionInProgress) {
-                  executeCommand();
+                  executeCommand()
                 }
               }}
             >
@@ -125,7 +125,7 @@ function App() {
         </For>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
